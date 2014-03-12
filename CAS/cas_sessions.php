76361f7f3ec907d9034be9d63632805fd9672483
Colby Warkentin <colby.warkentin@p2c.com>
2014-03-12 16:11:21 -0400

    Session cleanup

--- CAS/cas_sessions.php
+++ CAS/cas_sessions.php
@@ -4,7 +4,6 @@
     * functions for handling single signout *
     *****************************************/
   function _cas_single_signout_callback($logoutTicket) {
-    file_put_contents("/tmp/debug.log", "In CAS SIGNOUT $logoutTicket\n", FILE_APPEND);
     $mysqli = new mysqli(CONNECT_HOST, CONNECT_USER, CONNECT_PASSWD, CONNECT_DB);
     if (mysqli_connect_errno()) {
       throw new Exception($mysqli->connect_error);
@@ -17,15 +16,6 @@
         $mysqli->query("DELETE FROM cas_sessions WHERE sessionID = '" . $row['sessionID'] . "';");
       }
     }
-
-      /*$db = new PDO('sqlite:application.db');
-      $result = $db->query("SELECT sessionID from cas_login_data WHERE logoutTicket = '$logoutTicket';");
-      if ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-         unset($result);
-         $db->exec("DELETE FROM cas_login_data WHERE sessionID = '" . $row['sessionID'] . "';");
-         $db->exec("DELETE FROM sessions WHERE sessionID = '" . $row['sessionID'] . "';");
-      }*/
-
   }
 
   function _cas_track_logout_ticket($logoutTicket) {
@@ -37,14 +27,5 @@
     if ($result = $mysqli->query("CREATE TABLE IF NOT EXISTS cas_login_data (id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, sessionID CHAR(64), logoutTicket CHAR(64));")) {
       $mysqli->query("INSERT INTO cas_login_data (sessionID, logoutTicket) VALUES ('".session_id()."', '$logoutTicket');");
     }
-
-      // open SQLite sessions database and create cas_login_data table if it doesn't exist
-      /*if ($db = new PDO('sqlite:application.db')) {
-         $db->exec("CREATE TABLE IF NOT EXISTS cas_login_data (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, sessionID CHAR(26), logoutTicket CHAR(33));");
-      } else {
-         die('Unable to open database application.db');
-      }
-
-      $db->exec("INSERT INTO cas_login_data (sessionID, logoutTicket) VALUES ('".session_id()."', '$logoutTicket');");*/
   }
 ?>
--- CAS/sessions.php
+++ CAS/sessions.php
@@ -7,13 +7,6 @@
   else {
     $mysqli->query("CREATE TABLE IF NOT EXISTS cas_sessions (id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, sessionID CHAR(64), data TEXT DEFAULT '', dateTouched INTEGER);");
   }
-	
-  // open SQLite sessions database and create sessions table if it doesn't exist
-	//if ($db = new PDO('sqlite:application.db')) {
-	//		$db->exec("CREATE TABLE IF NOT EXISTS sessions (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, sessionID CHAR(26), data TEXT DEFAULT '', dateTouched INTEGER);");
-	// } else {
-	//		die('Unable to open database application.db');
-	// }
 
 	function sess_open($sess_path, $sess_name) {
     return true;
@@ -24,7 +17,6 @@
 	}
 
 	function sess_read($sess_id) {
-    file_put_contents("/tmp/debug.log", "In Session READ $sess_id\n", FILE_APPEND);
     $mysqli = new mysqli(CONNECT_HOST, CONNECT_USER, CONNECT_PASSWD, CONNECT_DB);
     if (mysqli_connect_errno()) {
       throw new Exception($mysqli->connect_error);
@@ -34,29 +26,15 @@
     if ($result = $mysqli->query("SELECT data FROM cas_sessions WHERE sessionID = '$sess_id';")) {
       if(!($row = mysqli_fetch_assoc($result))) {
         $mysqli->query("INSERT INTO cas_sessions (sessionID, dateTouched) VALUES ('$sess_id', $CurrentTime);");
-        file_put_contents("/tmp/debug.log", "In Session READ NOT FOUND\n", FILE_APPEND);
         return '';
       } else {
         $mysqli->query("UPDATE cas_sessions SET dateTouched = $CurrentTime WHERE sessionID = '$sess_id';");
-        file_put_contents("/tmp/debug.log", "In Session READ GOOD\n", FILE_APPEND);
         return $row['data'];
       }
     }
-
-		/*$db = new PDO('sqlite:application.db');
-		$result = $db->query("SELECT data FROM sessions WHERE sessionID = '$sess_id';");
-		$CurrentTime = time();
-		if (!$row = $result->fetch(PDO::FETCH_ASSOC)) {
-			 $db->exec("INSERT INTO sessions (sessionID, dateTouched) VALUES ('$sess_id', $CurrentTime);");
-			 return '';
-		} else {
-			 $db->exec("UPDATE sessions SET dateTouched = $CurrentTime WHERE sessionID = '$sess_id';");
-			 return $row['data'];
-		}*/
   }
 
 	function sess_write($sess_id, $data) {
-    file_put_contents("/tmp/debug.log", "In Session Write $sess_id : $data \n", FILE_APPEND);
     $mysqli = new mysqli(CONNECT_HOST, CONNECT_USER, CONNECT_PASSWD, CONNECT_DB);
     if (mysqli_connect_errno()) {
       throw new Exception($mysqli->connect_error);
@@ -66,12 +44,6 @@
       return true;
     }
     return true;
-
-
-		/*$db = new PDO('sqlite:application.db');
-		$CurrentTime = time();
-		$db->exec("UPDATE sessions SET data = '$data', dateTouched = $CurrentTime WHERE sessionID = '$sess_id';");
-		return true;*/
   }
 
   function sess_destroy($sess_id) {
@@ -84,11 +56,6 @@
       return true;
     }
     return true;
-
-
-			/*$db = new PDO('sqlite:application.db');
-			$db->exec("DELETE FROM sessions WHERE sessionID = '$sess_id';");
-			return true;*/
   }
 
   function sess_gc($sess_maxlifetime) {
@@ -101,11 +68,6 @@
       return true;
     }
     return false;
-
-			/*$db = new PDO('sqlite:application.db');
-			$CurrentTime = time();
-			$db->exec("DELETE FROM sessions WHERE dateTouched + $sess_maxlifetime < $CurrentTime;");
-			return true;*/
   }
 
 	 session_set_save_handler("sess_open", "sess_close", "sess_read", "sess_write", "sess_destroy", "sess_gc");
