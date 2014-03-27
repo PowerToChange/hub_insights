3982ac64981b56a322c4c4d3c3aa92b7e04f6326
Colby Warkentin <colby.warkentin@p2c.com>
2014-03-27 15:09:34 -0400

    Set favicon for pulse2

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
 <html>
   <head>
     <title><?php echo $title; ?></title>
+    <link rel="shortcut icon" href="/images/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset='utf-8'>
     <!-- Stylesheets -->
